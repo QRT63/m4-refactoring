7c1bce3780807e8fcc7ee5b3f5ef87a67c6a0a97
QRT63 <qiuru.tang@mail.utoronto.ca>
2025-11-21 15:31:33 -0500

    Completed Task 1.1

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,24 @@
 package theater;
 
+/**
+ * Class representing a play.
+ * @null This class does not return null values
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
     }
 }
